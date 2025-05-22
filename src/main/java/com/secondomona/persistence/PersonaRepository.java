fb1145820204791c63d5fc0c8395e33c77581bd6
RabbiHossain05 <rabbi.hossain@itsincom.it>
2025-05-22 12:20:36 +0200

    Create PersonaRepository

---
+++ src/main/java/com/secondomona/persistence/PersonaRepository.java
@@ -0,0 +1,31 @@
+package com.secondomona.persistence;
+
+import com.secondomona.persistence.model.Persona;
+import io.quarkus.elytron.security.common.BcryptUtil;
+import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
+import io.quarkus.panache.common.Parameters;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class PersonaRepository implements PanacheRepositoryBase<Persona, Long> {
+
+    public Persona authenticate(String email, String password) {
+        Persona persona = findByEmail(email);
+        if (persona == null) {
+            return null;
+        }
+        boolean matches = BcryptUtil.matches(password, persona.getPasswordHash());
+        if (matches) {
+            return persona;
+        }
+        return null;
+    }
+
+    private Persona findByEmail(String email) {
+        return find(
+                "SELECT p FROM Persona p WHERE " +
+                        "p.mail = :mail",
+                Parameters.with("mail", email)
+        ).firstResult();
+    }
+}
