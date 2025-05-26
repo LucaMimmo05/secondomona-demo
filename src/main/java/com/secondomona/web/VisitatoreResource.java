17034c84e1819e90fcf5d7624663d620edc78c6e
LucaMimmo <171295375+LucaMimmo05@users.noreply.github.com>
2025-05-26 15:37:21 +0200

    Changed Repository to Service

--- src/main/java/com/secondomona/web/VisitatoreResource.java
+++ src/main/java/com/secondomona/web/VisitatoreResource.java
@@ -2,7 +2,7 @@ package com.secondomona.web;
 
 import com.secondomona.persistence.PersonaRepository;
 import com.secondomona.persistence.model.Persona;
-import jakarta.annotation.security.RolesAllowed;
+import com.secondomona.service.PersonaService;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
@@ -15,11 +15,11 @@ import java.util.List;
 public class VisitatoreResource {
 
 
-    private final PersonaRepository personaRepository;
+    private final PersonaService personaService;
 
 
-    public VisitatoreResource(PersonaRepository personaRepository) {
-        this.personaRepository = personaRepository;
+    public VisitatoreResource(PersonaService personaRepository) {
+        this.personaService = personaRepository;
     }
 
 
@@ -28,7 +28,7 @@ public class VisitatoreResource {
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     public List<Persona> getVisitatori() {
-        return personaRepository.getVisitatori();
+        return personaService.getVisitatori();
     }
 
 }
